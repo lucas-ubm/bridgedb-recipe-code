2447ec885cba69578ccb6652c381304ff150c66b
Mavrokefalidis <lgubm23@gmail.com>
2020-11-29 10:10:38 +0100

    Added docs for script

--- .ipynb_checkpoints/bridgedb_script-checkpoint.py
+++ .ipynb_checkpoints/bridgedb_script-checkpoint.py
@@ -1,3 +1,7 @@
+""" Code for the FAIRplus cookbook identifier mapping with BridgeDB recipe
+
+"""
+
 import requests
 import pandas as pd
 
@@ -6,6 +10,25 @@ single_request = url+"{org}/xrefs/{source}/{identifier}"
 batch_request = url+"{org}/xrefsBatch/{source}"
 
 def to_df(response, batch=False):
+    """ Using a request object generates a pandas dataframe from the text response.
+    
+    Using the given response from BridgeDB's webservices we retrieve the data and split it into
+    4 columns. 
+    
+    Parameters
+    ----------
+    response : requests.Response object 
+        Response of the webservices after having performed a GET or POST call
+    batch: bool, optional
+        Whether the mapping is being done for only one identifier or multiple
+        
+    Returns
+    -------
+    result_df : pandas DataFrame
+        DataFrame containing the mappings in an easier to read and modify format
+        
+    """
+    
     if batch:
         records = []
         for tup in to_df(response).itertuples():
@@ -17,10 +40,35 @@ def to_df(response, batch=False):
         
     return pd.DataFrame([line.split('\t') for line in response.text.split('\n')])
 
-def get_mappings(file, org, source, case=1):
+def get_mappings(file, org, source, case = 1):
+    """ Calculates mappings of identifiers from organism `org` and `source` identifier
+    
+    This method calculates the mappings of identifiers stored in `file` corresponding to organism
+    `org`. If case == 1 then file should contain a TSV file with a list of identifiers from 
+    `source`. If case == 2 the file should contain a TSV file containing pairs of identifiers 
+    with the first element corresponding to a local identifier and the second one corresponding to
+    `source`.
+    
+    Parameters
+    ----------
+    file : str
+        Path to the TSV file containing the identifiers to be mapped
+    org : str
+        Name of the organism for which we are mapping
+    source : str
+        Name of the source dataset to which our identifiers correspond. It should be one of the
+        data sources accepted by BridgeDb [see here](shorturl.at/fvP24)
+    case : {1, 2}, optional
+        1 indicates we are trying to map from one of the available data sources. 2 indicates 
+        we are trying to map from a local identifier to a different data source using a pre-defined
+        mapping (e.g. we have a local identifier mapped to Ensembl and we want to use BridgeDb to 
+        map the local identifier to Entrez)
+    
+    """
+    
     names = ['source']
         
-    if case==2:
+    if case == 2:
         names = ['local'] + names
         
     data = pd.read_csv(file, sep='\t', header=None, names=names)
