3ac2197a910ce05dac2ec48d49fb6249213eabdb
Mavrokefalidis <lgubm23@gmail.com>
2021-01-04 09:51:50 +0100

    updated R mapping

--- identifier-mapping.r
+++ identifier-mapping.r
@@ -2,6 +2,11 @@ library("BridgeDbR")
 library("tidyverse")
 
 get_mappings <- function(filepath, organism, source, target='') {
+  # filepath indicates the path to the file containing the identifiers to be mapped
+  # organism is the organism these identifiers are describing
+  # source is the system code for the data source of the identifiers
+  # target is an optional parameter indicating the data source to which we are mapping
+
   data_df <- read_tsv(filepath, col_names=FALSE)
   if (dim(data_df)[2] == 1){
     colnames(data_df) <- c('identifier')
@@ -17,6 +22,10 @@ get_mappings <- function(filepath, organism, source, target='') {
   
   if (nchar(target) == 0) {
     mapping = maps(mapper, data_df)
+  }
+
+  else{
+    mapping = maps(mapper, data_df, target=target
   }
   return(right_join(data_df, mapping))
 }
